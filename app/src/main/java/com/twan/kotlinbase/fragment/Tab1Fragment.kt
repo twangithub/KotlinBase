e23de2fff0ae8181777bbb83a7a86eb9d5af98d1
Twan <599054904@qq.com>
2021-04-23 10:50:41 +0800

    增加了ViewModel示例

--- app/src/main/java/com/twan/kotlinbase/fragment/Tab1Fragment.kt
+++ app/src/main/java/com/twan/kotlinbase/fragment/Tab1Fragment.kt
@@ -2,20 +2,23 @@ package com.twan.kotlinbase.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
-import butterknife.BindView
-import com.scwang.smart.refresh.layout.SmartRefreshLayout
 import com.twan.kotlinbase.R
 import com.twan.kotlinbase.app.BaseFragment
 import com.twan.kotlinbase.databinding.Tab1FragmentBinding
+import com.twan.kotlinbase.viewmodel.Tab1ViewModel
 import com.twan.kotlinbase.network.Response
 import com.twan.kotlinbase.network.RxHttpScope
+import kotlinx.android.synthetic.main.tab1_fragment.*
 import rxhttp.toClass
 import rxhttp.wrapper.param.RxHttp
 
 
 class Tab1Fragment: BaseFragment<Tab1FragmentBinding>() {
-    @BindView(R.id.refreshLayout) lateinit var refreshLayout: SmartRefreshLayout
+
+    //viewmodel使用示例
+    private val myViewModel by viewModels<Tab1ViewModel>()
 
     override fun getLayoutId(): Int {
         return R.layout.tab1_fragment
@@ -25,6 +28,13 @@ class Tab1Fragment: BaseFragment<Tab1FragmentBinding>() {
         title?.text = resources.getText(R.string.tab1)
         back?.visibility=View.GONE
 
+
+        //使用viewmodel后,屏幕旋转后initView重新被调用,但是myViewModel数据不会丢失
+        tv_num.text = myViewModel.number.toString()
+        btn_add.setOnClickListener {
+            myViewModel.number++
+            tv_num.text = myViewModel.number.toString()
+        }
     }
 
     fun getDataFromNetwork() {
